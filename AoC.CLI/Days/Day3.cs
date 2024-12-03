77e3953b9c164defd31519ba8d9cf614f79849a5
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-03 21:27:24 +0100

    Formatting

--- AoC.CLI/Days/Day3.cs
+++ AoC.CLI/Days/Day3.cs
@@ -5,77 +5,79 @@ namespace AoC.CLI.Days;
 
 internal static partial class CustomRegex
 {
-    [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)")]
-    public static partial Regex Part1Regex();
+  [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)")]
+  public static partial Regex Part1Regex();
 
-    [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)")]
-    public static partial Regex Part2Regex();
+  [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)")]
+  public static partial Regex Part2Regex();
 }
 
 internal class Day3
     : IDay
 {
-    public Task<string> ExecutePart1(
-        IEnumerable<string> input)
+  public Task<string> ExecutePart1(
+      IEnumerable<string> input)
+  {
+    var builder = new StringBuilder();
+
+    foreach (var line in input)
     {
-        var builder = new StringBuilder();
-
-        foreach(var line in input)
-        {
-            builder.Append(line);
-        }
-
-        MatchCollection matches = CustomRegex.Part1Regex()
-            .Matches(builder.ToString());
-
-        var answer = matches
-            .Select(x => {
-                var instructions = x.ToString().Replace("mul(", "").Replace(")", "").Split(',');
-                return int.Parse(instructions[0]) * int.Parse(instructions[1]);
-            })
-            .Sum();
-
-        return Task.FromResult(answer.ToString());
+      builder.Append(line);
     }
 
-    public Task<string> ExecutePart2(
-        IEnumerable<string> input)
-    {
-        var builder = new StringBuilder();
+    MatchCollection matches = CustomRegex.Part1Regex()
+        .Matches(builder.ToString());
 
-        foreach(var line in input)
+    var answer = matches
+        .Select(x =>
         {
-            builder.Append(line);
-        }
+          var instructions = x.ToString().Replace("mul(", "").Replace(")", "").Split(',');
+          return int.Parse(instructions[0]) * int.Parse(instructions[1]);
+        })
+        .Sum();
 
-        MatchCollection matches = CustomRegex.Part2Regex()
-            .Matches(builder.ToString());
+    return Task.FromResult(answer.ToString());
+  }
 
-        var doCalculation = true;
+  public Task<string> ExecutePart2(
+      IEnumerable<string> input)
+  {
+    var builder = new StringBuilder();
 
-        var answer = matches
-            .Select(x => {
-                if(x.ToString() == "do()")
-                {
-                    doCalculation = true;
-                    return 0;
-                }
-                else if(x.ToString() == "don't()")
-                {
-                    doCalculation = false;
-                    return 0;
-                }
-
-                if(!doCalculation)
-                {
-                    return 0;
-                }
-                
-                var instructions = x.ToString().Replace("mul(", "").Replace(")", "").Split(',');
-                return int.Parse(instructions[0]) * int.Parse(instructions[1]);
-            })
-            .Sum();
-
-        return Task.FromResult(answer.ToString());
+    foreach (var line in input)
+    {
+      builder.Append(line);
     }
+
+    MatchCollection matches = CustomRegex.Part2Regex()
+        .Matches(builder.ToString());
+
+    var doCalculation = true;
+
+    var answer = matches
+        .Select(x =>
+        {
+          if (x.ToString() == "do()")
+          {
+            doCalculation = true;
+            return 0;
+          }
+          else if (x.ToString() == "don't()")
+          {
+            doCalculation = false;
+            return 0;
+          }
+
+          if (!doCalculation)
+          {
+            return 0;
+          }
+
+          var instructions = x.ToString().Replace("mul(", "").Replace(")", "").Split(',');
+          return int.Parse(instructions[0]) * int.Parse(instructions[1]);
+        })
+        .Sum();
+
+    return Task.FromResult(answer.ToString());
+  }
 }
