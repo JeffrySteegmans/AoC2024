444b53d695e313ab54ba892598a09b3390c84a31
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-03 14:50:30 +0100

    Day2 - Part 2

--- AoC.CLI/Days/Day2.cs
+++ AoC.CLI/Days/Day2.cs
@@ -54,6 +54,67 @@ internal class Day2
     public Task<string> ExecutePart2(
         IEnumerable<string> input)
     {
-        return Task.FromResult("Part 2 not implemented");
+        var answer = input
+            .Count(IsSaveWithDampener);
+
+        return Task.FromResult(answer.ToString());
+    }
+
+    private bool IsSaveWithDampener(
+        string report)
+    {
+        var parts = report
+            .Split(' ')
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        if (CheckPart(parts)) {
+            return true;
+        }
+
+        for (var i = 0; i < parts.Count; i++){
+            var newList = parts
+                .Where((x, index) => index != i)
+                .ToList();
+
+            if (CheckPart(newList)) {
+                return true;
+            }
+        }
+
+        return false;        
+    }
+
+    private bool CheckPart(
+        List<int> parts
+    ) {
+        bool? increasing = null;
+
+        for (int i = 1; i < parts.Count; i++)
+        {
+            var difference = Math.Abs(parts[i] - parts[i - 1]);
+            if (difference > 3 || difference < 1) {
+                return false;
+            }
+
+            if (parts[i] > parts[i - 1])
+            {
+                if (increasing == false)
+                {
+                    return false; // Was decreasing, now increasing
+                }
+                increasing = true;
+            }
+            else if (parts[i] < parts[i - 1])
+            {
+                if (increasing == true)
+                {
+                    return false; // Was increasing, now decreasing
+                }
+                increasing = false;
+            }
+        }
+
+        return true;
     }
 }
