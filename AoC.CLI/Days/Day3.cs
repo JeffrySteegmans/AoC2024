b76c89714c5eb7ee7ceb8a22edac012629ce964a
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-03 21:10:28 +0100

    Day 3 - Part 1

--- AoC.CLI/Days/Day3.cs
+++ AoC.CLI/Days/Day3.cs
@@ -1,6 +1,13 @@
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace AoC.CLI.Days;
+
+internal static partial class CustomRegex
+{
+    [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)")]
+    public static partial Regex MulRegex();
+}
 
 internal class Day3
     : IDay
@@ -10,16 +17,33 @@ internal class Day3
     public Task<string> ExecutePart1(
         IEnumerable<string> input)
     {
-        var regex = new Regex(pattern);
+        var builder = new StringBuilder();
 
-        Match m = regex.Match(input.First());
+        foreach(var line in input)
+        {
+            builder.Append(line);
+        }
 
-        return Task.FromResult(m.Groups.Count.ToString());
+        MatchCollection matches = CustomRegex.MulRegex()
+            .Matches(builder.ToString());
+
+        var answer = matches
+            .Select(x => {
+                var instructions = x.ToString().Replace("mul(", "").Replace(")", "").Split(',');
+                return int.Parse(instructions[0]) * int.Parse(instructions[1]);
+            })
+            .Sum();
+
+        return Task.FromResult(answer.ToString());
     }
 
     public Task<string> ExecutePart2(
         IEnumerable<string> input)
     {
-        return Task.FromResult(String.Join("-", input));
+        var regex = new Regex(pattern);
+
+        Match m = regex.Match(input.First());
+
+        return Task.FromResult(m.Groups.Count.ToString());
     }
 }
