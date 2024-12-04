7a8f4a42cd99e6aa640f232069d957cf440f8ed2
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-04 08:39:04 +0100

    Day 4 - Part 2

--- AoC.CLI/Days/Day4.cs
+++ AoC.CLI/Days/Day4.cs
@@ -105,9 +105,77 @@ internal class Day4
       return matrix;  
     }
 
-    public Task<string> ExecutePart2(
+    public async Task<string> ExecutePart2(
       IEnumerable<string> input)
     {
-        return Task.FromResult("Not implemented");
+      var matrix = CreateMatrix(input);
+      
+      var answer = await SearchAllXmas(
+        matrix);
+
+      return answer
+        .ToString();
+    }
+
+    private async Task<int> SearchAllXmas(
+      char[,] matrix) 
+    {
+      var total = 0;
+
+      for (int i = 0; i < matrix.GetLength(0); i++) 
+      {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+          var character = matrix[i, j];
+          
+          if (character != 'A') {
+            continue;
+          }
+
+          var xmasFound = await SearchXmas(
+            matrix,
+            i,
+            j
+          );
+          if (xmasFound) {
+            total++;
+          }
+        }
+      }
+
+      return total;
+    }
+
+    private Task<bool> SearchXmas(
+      char[,] matrix,
+      int rowIndex,
+      int colIndex)
+    {
+      var rowLength = matrix.GetLength(0);
+      var colLength = matrix.GetLength(1);
+
+      if (OutOfBound(rowLength, colLength, rowIndex, colIndex)) {
+        return Task.FromResult(false);
+      }
+
+      var leftUpCharacter = matrix[rowIndex-1, colIndex-1];
+      var rightUpCharacter = matrix[rowIndex-1, colIndex+1];
+      var leftDownCharacter = matrix[rowIndex+1, colIndex-1];
+      var rightDownCharacter = matrix[rowIndex+1, colIndex+1];
+
+      return Task.FromResult(IsMas(leftUpCharacter, rightDownCharacter) && IsMas(rightUpCharacter, leftDownCharacter));
+
+      static bool IsMas(char firstCharacter, char secondCharacter)
+      {
+        if (firstCharacter == secondCharacter){
+          return false;
+        }
+
+        return (firstCharacter == 'M' && secondCharacter == 'S') || (firstCharacter == 'S' && secondCharacter == 'M');
+      }
+      static bool OutOfBound(int rowLength, int colLength, int rowIndex, int colIndex) 
+      {
+        return rowIndex == 0 || colIndex == 0 || rowIndex >= rowLength - 1 || colIndex >= colLength - 1;
+      }
     }
 }
