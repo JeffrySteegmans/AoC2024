2d67e5649f0d217195af206058e6ba218a348fd3
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-04 08:41:07 +0100

    Day 5 setup

--- AoC.CLI/Program.cs
+++ AoC.CLI/Program.cs
@@ -7,7 +7,8 @@ Dictionary<int, IDay> days = new()
     { 1, new Day1() },
     { 2, new Day2() },
     { 3, new Day3() },
-    { 4, new Day4() }
+    { 4, new Day4() },
+    { 5, new Day5() }
 };
 
 Console.Write("Day: ");
