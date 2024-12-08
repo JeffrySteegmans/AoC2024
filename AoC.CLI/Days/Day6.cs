bf8f97d23f0b41749cc67fcb2e988e6774702d0a
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-08 22:32:24 +0100

    Refactor InputExtensions

--- AoC.CLI/Days/Day6.cs
+++ AoC.CLI/Days/Day6.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC.CLI.Days;
+﻿using AoC.CLI.Extensions;
+
+namespace AoC.CLI.Days;
 
 internal class Day6
     : IDay
@@ -27,24 +29,6 @@ internal class Day6
 
 internal static class Day6Extensions
 {
-    public static char[,] ParseMap(
-        this IEnumerable<string> input)
-    {
-        var inputArray = input.ToArray();
-        var map = new char[inputArray[0].Length, inputArray.Length];
-
-        for (var rowIndex = 0; rowIndex < inputArray.Length; rowIndex++)
-        {
-            var line = inputArray[rowIndex].ToCharArray();
-            for (var colIndex = 0; colIndex < line.Length; colIndex++)
-            {
-                map[rowIndex, colIndex] = line[colIndex];
-            }
-        }
-
-        return map;
-    }
-
     public static Coordinate FindStart(
         this char[,] map)
     {
---
+++ AoC.CLI/Extensions/InputExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC.CLI.Extensions;
+
+internal static class InputExtensions
+{
+    public static char[,] ParseMap(
+        this IEnumerable<string> input)
+    {
+        var inputArray = input.ToArray();
+        var map = new char[inputArray[0].Length, inputArray.Length];
+
+        for (var rowIndex = 0; rowIndex < inputArray.Length; rowIndex++)
+        {
+            var line = inputArray[rowIndex].ToCharArray();
+            for (var colIndex = 0; colIndex < line.Length; colIndex++)
+            {
+                map[rowIndex, colIndex] = line[colIndex];
+            }
+        }
+
+        return map;
+    }
+}
