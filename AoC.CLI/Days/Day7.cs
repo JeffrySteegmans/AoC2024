c54b320595897f55e7be91adcb1a253679d0211d
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-08 20:18:38 +0100

    Day 7 setup

---
+++ AoC.CLI/Days/Day7.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC.CLI.Days;
+
+internal class Day7
+    : IDay
+{
+    public Task<string> ExecutePart1(
+        IEnumerable<string> input)
+    {
+        return Task.FromResult("Not implemented yet");
+    }
+
+    public Task<string> ExecutePart2(
+        IEnumerable<string> input)
+    {
+        return Task.FromResult("Not implemented yet");
+    }
+}
--- AoC.CLI/Program.cs
+++ AoC.CLI/Program.cs
@@ -10,7 +10,8 @@ Dictionary<int, IDay> days = new()
     { 3, new Day3() },
     { 4, new Day4() },
     { 5, new Day5() },
-    { 6, new Day6() }
+    { 6, new Day6() },
+    { 7, new Day7() }
 };
 
 Console.Write("Day: ");
