46d9922f386e945db54008d16ca0136ffb83dca9
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-09 21:40:05 +0100

    Day9 setup

---
+++ AoC.CLI/Days/Day9.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC.CLI.Days;
+
+public class Day9
+    : IDay
+{
+    public Task<string> ExecutePart1(
+        IEnumerable<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> ExecutePart2(
+        IEnumerable<string> input)
+    {
+        throw new NotImplementedException();
+    }
+}
--- AoC.CLI/Program.cs
+++ AoC.CLI/Program.cs
@@ -12,7 +12,8 @@ Dictionary<int, IDay> days = new()
     { 5, new Day5() },
     { 6, new Day6() },
     { 7, new Day7() },
-    { 8, new Day8() }
+    { 8, new Day8() },
+    { 9, new Day9() }
 };
 
 Console.Write("Day: ");
