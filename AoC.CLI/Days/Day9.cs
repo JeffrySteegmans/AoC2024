82b034b724c28ee24d7e47466163241d4902e3b2
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-11 09:19:24 +0100

    Day 9 - Part 2

--- AoC.CLI/Days/Day9.cs
+++ AoC.CLI/Days/Day9.cs
@@ -1,4 +1,4 @@
-﻿using System.Numerics;
+﻿using System.Text;
 
 namespace AoC.CLI.Days;
 
@@ -21,8 +21,21 @@ public class Day9
     public Task<string> ExecutePart2(
         IEnumerable<string> input)
     {
-        return Task.FromResult("Not implemented");
+        var answer = input
+            .CreateDiskMap()
+            .GenerateStorage()
+            .Defragment()
+            .ToFileSystemString()
+            .CalculateChecksum()
+            .ToString();
+
+        return Task.FromResult(answer);
     }
+}
+
+internal static class Constants
+{
+    public const int FreeSpaceFileId = -1;
 }
 
 internal static class Day9Extensions
@@ -36,6 +49,131 @@ internal static class Day9Extensions
             .ToList();
 
         return diskMap;
+    }
+
+    public static List<Block> GenerateStorage(
+        this List<int> diskMap)
+    {
+        var blocks = new List<Block>();
+        var fileId = 0;
+
+        for(var i = 0; i < diskMap.Count; i++)
+        {
+            var size = diskMap[i];
+
+            if (size == 0)
+            {
+                continue;
+            }
+
+            var block = new Block(
+                i % 2 == 0 ? fileId++ : Constants.FreeSpaceFileId,
+                size);
+
+            blocks
+                .Add(block);
+        }
+
+        return blocks;
+    }
+
+    public static List<Block> Defragment(
+        this List<Block> storage)
+    {
+        List<Block> defragmented = [..storage];
+
+        var files = storage
+            .Where(x => x.FileId != Constants.FreeSpaceFileId)
+            .Reverse();
+
+        foreach (var file in files)
+        {
+            var freeSpace = defragmented
+                .FirstOrDefault(x => x.FileId == Constants.FreeSpaceFileId && x.Size >= file.Size);
+
+            if (freeSpace is not null)
+            {
+                var rightIndex = defragmented.IndexOf(file);
+                var leftIndex = defragmented.IndexOf(freeSpace);
+
+                if (leftIndex >= rightIndex)
+                {
+                    continue;
+                }
+
+                (defragmented[rightIndex], defragmented[leftIndex]) = (defragmented[leftIndex], defragmented[rightIndex]);
+
+                if (freeSpace.Size <= file.Size)
+                {
+                    continue;
+                }
+
+                var newFreeSpaceSize = freeSpace.Size - file.Size;
+
+                defragmented[rightIndex] = defragmented[rightIndex] with { Size = file.Size };
+
+                var newFreeSpace = new Block(Constants.FreeSpaceFileId, newFreeSpaceSize);
+                defragmented
+                    .Insert(leftIndex + 1, newFreeSpace);
+
+                defragmented = defragmented
+                    .ConsolidateFreeSpace();
+            }
+        }
+
+        return defragmented;
+    }
+
+    private static List<Block> ConsolidateFreeSpace(
+        this List<Block> storage)
+    {
+        List<Block> consolidated = [..storage];
+
+        for (var i = 0; i < consolidated.Count; i++)
+        {
+            if (i > 0)
+            {
+                consolidated.ConsolidateFreeSpace(
+                    i - 1,
+                    i);
+            }
+
+            if (i < consolidated.Count - 1)
+            {
+                consolidated.ConsolidateFreeSpace(
+                    i,
+                    i + 1);
+            }
+        }
+
+        return consolidated;
+    }
+
+    private static void ConsolidateFreeSpace(
+        this List<Block> storage,
+        int leftIndex,
+        int rightIndex)
+    {
+        var leftBlock = storage[leftIndex];
+        var rightBlock = storage[rightIndex];
+
+        if (leftBlock.FileId != Constants.FreeSpaceFileId || rightBlock.FileId != Constants.FreeSpaceFileId)
+        {
+            return;
+        }
+
+        storage[leftIndex] = leftBlock with { Size = leftBlock.Size + rightBlock.Size };
+        storage
+            .RemoveAt(rightIndex);
+    }
+
+    public static List<string> ToFileSystemString(
+        this List<Block> storage)
+    {
+        return storage
+            .Select(x => x.ToFileSystem())
+            .SelectMany(x => x)
+            .ToList();
     }
 
     public static List<string> GenerateBlocks(
@@ -101,19 +239,141 @@ internal static class Day9Extensions
         return defragmentedBlocks;
     }
 
+    public static List<string> DefragmentFiles(
+        List<string> fragmentedBlocks,
+        string lastFileId)
+    {
+        List<string> defragmentedBlocks = [..fragmentedBlocks];
+
+        var fileIndexes = defragmentedBlocks
+            .GetFileIndexes(lastFileId);
+
+        var freeSpaceIndexes = defragmentedBlocks
+            .GetFreeSpaceIndexes(fileIndexes.Count);
+
+        var currentFile = defragmentedBlocks[fileIndexes[0]];
+
+        if (!freeSpaceIndexes.Any())
+        {
+            return DefragmentFiles(defragmentedBlocks, currentFile);
+        }
+
+        if (freeSpaceIndexes.First() >= fileIndexes.First())
+        {
+            return defragmentedBlocks;
+        }
+
+        for (var i = 0; i < fileIndexes.Count; i++)
+        {
+            var freeSpace = freeSpaceIndexes[i];
+            var file = fileIndexes[i];
+
+            (defragmentedBlocks[freeSpace], defragmentedBlocks[file]) = (defragmentedBlocks[file], defragmentedBlocks[freeSpace]);
+        }
+
+        return DefragmentFiles(defragmentedBlocks, currentFile);
+    }
+
+    private static List<int> GetFileIndexes(
+        this List<string> blocks,
+        string lastFileId)
+    {
+        var indexes = new List<int>();
+        var currentFile = string.Empty;
+        var lastFilePassed = lastFileId == string.Empty;
+
+        for (var i = blocks.Count - 1; i >= 0; i--)
+        {
+            if (blocks[i] == ".")
+            {
+                continue;
+            }
+
+            if (blocks[i] == lastFileId)
+            {
+                lastFilePassed = true;
+                continue;
+            }
+
+            if (lastFilePassed)
+            {
+                if (currentFile == string.Empty || currentFile == blocks[i])
+                {
+                    currentFile = blocks[i];
+                    indexes.Add(i);
+                }
+
+                if (currentFile != blocks[i])
+                {
+                    break;
+                }
+            }
+        }
+
+        return indexes;
+    }
+
+    private static List<int> GetFreeSpaceIndexes(
+        this List<string> blocks,
+        int fileSize)
+    {
+        var freeSpaceIndexes = new List<int>();
+
+        for (var i = 0; i <= blocks.Count - 1; i++)
+        {
+            if (blocks[i] != ".")
+            {
+                freeSpaceIndexes = [];
+                continue;
+            }
+
+            if (blocks[i] == ".")
+            {
+                freeSpaceIndexes.Add(i);
+            }
+
+            if (freeSpaceIndexes.Count == fileSize)
+            {
+                break;
+            }
+        }
+
+        return freeSpaceIndexes.Count == fileSize ?
+            freeSpaceIndexes :
+            [];
+    }
+
     public static long CalculateChecksum(
         this List<string> blocks)
     {
-        var multipliedBlocks = blocks
-            .Where(x => x != ".")
-            .Select((x, index) => int.Parse(x) * index);
-
         long sum = 0;
-        foreach (var b in multipliedBlocks)
+        for (var i = 0; i < blocks.Count; i++)
         {
-            sum += b;
+            if (blocks[i] == ".")
+            {
+                continue;
+            }
+
+            sum += int.Parse(blocks[i]) * i;
         }
 
         return sum;
     }
 }
+
+internal record Block(
+    int FileId,
+    int Size)
+{
+    public List<string> ToFileSystem()
+    {
+        var fileSystem = new List<string>();
+
+        for (var i = 0; i < Size; i++)
+        {
+            fileSystem.Add(FileId == -1 ? "." : FileId.ToString());
+        }
+
+        return fileSystem;
+    }
+};
