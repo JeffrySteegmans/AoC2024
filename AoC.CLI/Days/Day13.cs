f809f394066a5ed83de13e28e9269cffe438cd6c
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-12-15 22:11:33 +0100

    Day 13 setup

---
+++ AoC.CLI/Days/Day13.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC.CLI.Days;
+
+public class Day13
+    : IDay
+{
+    public Task<string> ExecutePart1(
+        IEnumerable<string> input)
+    {
+        return Task.FromResult("No implemented");
+    }
+
+    public Task<string> ExecutePart2(
+        IEnumerable<string> input)
+    {
+        return Task.FromResult("No implemented");
+    }
+}
